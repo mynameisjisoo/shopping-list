9aaf8072dcc0354464b091ef971c57146c4cc3ca
mynameisjisoo <devjisoolee@gmail.com>
2021-09-04 23:55:56 +0900

    전체 코드 수정 * HTML 섹션 구조 변경 * js 성능 개선 및 가독성 위해  코드 가동 방식 변경 * js remove 기능 동작 안해서 수정 예정

--- main.js
+++ main.js
@@ -1,47 +1,51 @@
 'use strict';
-const shoppingCart = document.querySelector('#shoppingCart');
-const text = document.querySelector('#text');
-const ul = document.querySelector('ul');
-const imgsrc = './img/trash.png';
+const items = document.querySelector('.items');
+const btn = document.querySelector('.footer_button');
+const input = document.querySelector('.footer_input');
 
-//클릭으로 입력
-shoppingCart.addEventListener('click', () => {
-  //공백이아닐때만 추가하기
-  if (text.value != '') {
-    addShoppingList();
+function onAdd() {
+  const text = input.value;
+  if (text === '') {
+    input.focus();
+    return;
   }
-});
 
-//엔터로 입력
-text.addEventListener('keydown', event => {
-  if (text.value != '') {
-    if (event.key == 'Enter') {
-      addShoppingList();
-    }
-  }
-});
-
-function addShoppingList() {
-  const item = text.value; //사용자가 입력한 내용
-  const li = document.createElement('li');
-  li.textContent = item;
-  ul.appendChild(li);
-
-  //img의 id값 구하기
-  const garbageIcon = `${item.replace(/ /g, '')}${ul.childElementCount}`; //입력내용공백제거,번호 붙여서 쓰레기통아이콘 id로 설정
-  //li에 이미지 넣기
-  const img = document.createElement('img');
-  img.setAttribute('class', 'trash');
-  img.setAttribute('id', garbageIcon);
-  img.src = imgsrc;
-  img.alt = 'trash';
-  li.appendChild(img);
-  text.value = ''; //입력창 초기화
-
-  //쓰레기통 눌렀을 때
-  const remove = document.querySelector(`#${garbageIcon}`);
-  remove.addEventListener('click', () => {
-    remove.parentElement.remove();
-  });
+  items.appendChild(item);
+  input.value = '';
+  input.focus();
+  //   item.scrollIntoView();
 }
-``;
+
+function createItem(text) {
+  const item = document.createElement('li');
+  item.setAttribute('class', 'item');
+  item.innerText = text;
+
+  const divider = document.createElement('div');
+  divider.setAttribute('class', 'divider');
+
+  const item_delete = document.createElement('button');
+  item_delete.setAttribute('class', 'item_delete');
+  item_delete.innerHTML = "<i class='fas fa-trash-alt'></i>";
+
+  item_delete.addEventListener('click', () => {
+    console.log(item_delete.parentElement);
+    items.removeChild(item);
+  });
+
+  items.appendChild(item);
+  item.appendChild(item_delete);
+  items.appendChild(divider);
+
+  return item;
+}
+
+input.addEventListener('keypress', event => {
+  if (event.key === 'Enter') {
+  }
+  onAdd();
+});
+
+btn.addEventListener('click', () => {
+  onAdd();
+});
