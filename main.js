78169b1e37c26a345728eefe57b8823a3e6e455e
mynameisjisoo <devjisoolee@gmail.com>
2021-09-05 00:29:40 +0900

    전체삭제 기능 추가

--- main.js
+++ main.js
@@ -9,41 +9,56 @@ function onAdd() {
     input.focus();
     return;
   }
+  const item = createItem(text);
 
   items.appendChild(item);
   input.value = '';
   input.focus();
-  //   item.scrollIntoView();
+  item.scrollIntoView({ block: 'center' });
 }
 
 function createItem(text) {
-  const item = document.createElement('li');
-  item.setAttribute('class', 'item');
-  item.innerText = text;
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'itemRow');
+  //   item.innerText = text;
 
-  const divider = document.createElement('div');
-  divider.setAttribute('class', 'divider');
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
+
+  const name = document.createElement('span');
+  name.setAttribute('class', 'item_name');
+  name.innerHTML = text;
 
   const item_delete = document.createElement('button');
   item_delete.setAttribute('class', 'item_delete');
   item_delete.innerHTML = "<i class='fas fa-trash-alt'></i>";
 
+  const divider = document.createElement('div');
+  divider.setAttribute('class', 'divider');
+
+  const item_divider = document.createElement('div');
+  item_divider.setAttribute('class', 'item_divider');
+
+  item.appendChild(name);
+  item.append(item_delete);
+  itemRow.appendChild(item);
+  itemRow.appendChild(item_divider);
+
   item_delete.addEventListener('click', () => {
-    console.log(item_delete.parentElement);
-    items.removeChild(item);
+    items.removeChild(itemRow);
   });
 
-  items.appendChild(item);
-  item.appendChild(item_delete);
-  items.appendChild(divider);
-
-  return item;
+  const delete_All = document.querySelector('.footer_deleteAll');
+  delete_All.addEventListener('click', () => {
+    items.removeChild(itemRow);
+  });
+  return itemRow;
 }
 
 input.addEventListener('keypress', event => {
   if (event.key === 'Enter') {
+    onAdd();
   }
-  onAdd();
 });
 
 btn.addEventListener('click', () => {
