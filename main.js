3109f7b3200ff9c57e987d8bca528d5147caac90
mynameisjisoo <devjisoolee@gmail.com>
2021-09-05 14:37:38 +0900

    Refator : change class name in HTML tag
    네이밍규칙에 맞춰 selector 이름 수정함.
    가독성을 위해 코드 순서 및 줄 맞춤 변경함

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 'use strict';
 const items = document.querySelector('.items');
-const btn = document.querySelector('.footer_button');
-const input = document.querySelector('.footer_input');
+const btn = document.querySelector('.footer__button');
+const input = document.querySelector('.footer__input');
 
 function onAdd() {
   const text = input.value;
+
   if (text === '') {
     input.focus();
     return;
   }
-  const item = createItem(text);
 
+  const item = createItem(text);
   items.appendChild(item);
   input.value = '';
   input.focus();
@@ -20,27 +21,23 @@ function onAdd() {
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'itemRow');
-  //   item.innerText = text;
 
   const item = document.createElement('div');
   item.setAttribute('class', 'item');
 
   const name = document.createElement('span');
-  name.setAttribute('class', 'item_name');
+  name.setAttribute('class', 'item__name');
   name.innerHTML = text;
 
   const item_delete = document.createElement('button');
-  item_delete.setAttribute('class', 'item_delete');
+  item_delete.setAttribute('class', 'item__delete');
   item_delete.innerHTML = "<i class='fas fa-trash-alt'></i>";
 
-  const divider = document.createElement('div');
-  divider.setAttribute('class', 'divider');
-
   const item_divider = document.createElement('div');
-  item_divider.setAttribute('class', 'item_divider');
+  item_divider.setAttribute('class', 'item__divider');
 
   item.appendChild(name);
-  item.append(item_delete);
+  item.appendChild(item_delete);
   itemRow.appendChild(item);
   itemRow.appendChild(item_divider);
 
@@ -48,10 +45,11 @@ function createItem(text) {
     items.removeChild(itemRow);
   });
 
-  const delete_All = document.querySelector('.footer_deleteAll');
+  const delete_All = document.querySelector('.footer__deleteAll');
   delete_All.addEventListener('click', () => {
     items.removeChild(itemRow);
   });
+
   return itemRow;
 }
 
